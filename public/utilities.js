d53cafea940a5c1fe771fe424306fbdff97580a3
mohdbinharun56 <arahaman461@gmail.com>
2024-10-22 00:37:11 +0600

    home section home container has been completed

--- public/utilities.js
+++ public/utilities.js
@@ -2,3 +2,19 @@ function toggleMenu(){
     document.getElementById('navigationBarMobile').classList.toggle('hidden');
     document.getElementById('registerNavigationBarMobile').classList.toggle('hidden');
 }
+
+
+// function home  (){
+//     const homeElement = document.createElement('div');
+//     homeElement.classList.add('homeContainer');
+
+//     homeElement.innerHTML = `
+//     <h1 class='font-semibold text-6xl text-white'>The Tulum Marketing & <span class='text-[#D9B742]'>Creative Agency</span></h1>
+//     `;
+
+//     document.getElementById('homeSection').appendChild(homeElement);
+
+// }
+
+
+// export default home;
